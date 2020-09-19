88aef30655f1aae93e941ffaf584571bcc86f422
yanwr <yanywr@gmai.com>
2020-09-19 18:46:59 -0300

    #4 Delete some comments and lines

--- be/src/controllers/UserController.ts
+++ be/src/controllers/UserController.ts
@@ -9,25 +9,17 @@ export default class UserController {
     async index(request:Request, response:Response) {};
 
     async show(request:Request, response:Response) {
-        try {
-            const userId = request.params.id;
-            const currentUser:User[] = await userService.show(userId);
-            return response.json(currentUser);
-        } catch (error) {
-            
-        }
+        const userId = request.params.id;
+        const currentUser:User[] = await userService.show(userId);
+        return response.json(currentUser);
     };
 
     async store(request:Request, response:Response) {
-        try {
-            const { name, email, whatsapp, password, id = null } = request.body;
-            const user:User = { id, name, email, whatsapp, password };
-            const userDTO = await userService.store(user);
-            return response.json(userDTO);
-        } catch (error) {
-            
-        }
-    }
+        const { name, email, whatsapp, password, id = null } = request.body;
+        const user:User = { id, name, email, whatsapp, password };
+        const userDTO = await userService.store(user);
+        return response.json(userDTO);
+    };
 
     async update(request:Request, response:Response) {};
 
